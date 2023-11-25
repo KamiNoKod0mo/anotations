915e09aabd43eba0d24dc17a76179769404a9cfb
NightMare126 <carlos.farias1267@gmail.com>
2023-11-25 11:46:28 -0300

    Comp

---
+++ c++/autocompC.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+arg1=$1
+arg2=$2
+name=`ls $arg2 | cut -d "." -f 1`
+if [ $arg1 != "--gcc" ] && [ $arg1 != "--g++" ];then
+	echo "Usage: "
+	echo "$0 --[gcc/g++] file"
+	exit
+fi
+if [ $arg1 == "--gcc" ];then
+	ch="gcc"
+else 
+	ch="g++"
+fi
+$ch $arg2 -o $name.c 
+sha=`md5sum $arg2 | cut -d " " -f 1`
+while true; do
+	if [ `md5sum $arg2 | cut -d " " -f 1` != $sha ];then
+		clear
+		$ch $arg2 -o $name.c
+		sha=`md5sum $arg2 | cut -d " " -f 1` 
+	fi
+done
